bef29021c4d5aedb41d19b0da83789f9c0f575e7
Arye <72888630+Ary428@users.noreply.github.com>
2025-05-13 22:11:03 +0300

    import IntroductionModule into AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { IntroductionModule } from './introduction/introduction.module';
 
 @Module({
-  imports: [],
+  imports: [IntroductionModule],
   controllers: [AppController],
   providers: [AppService],
 })

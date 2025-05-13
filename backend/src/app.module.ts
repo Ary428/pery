02d41185cf501da1dc6596fb6b8cbcc14583bed1
Arye <72888630+Ary428@users.noreply.github.com>
2025-05-13 22:32:41 +0300

    add UserModule to app.module imports

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { IntroductionModule } from './introduction/introduction.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [IntroductionModule],
+  imports: [IntroductionModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })

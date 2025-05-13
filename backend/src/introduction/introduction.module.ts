6b051d366e35ffbde824ebf35372b99ef82d9fc1
Arye <72888630+Ary428@users.noreply.github.com>
2025-05-13 22:42:42 +0300

    import UserModule into IntroductionModule

--- backend/src/introduction/introduction.module.ts
+++ backend/src/introduction/introduction.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { IntroductionController } from './introduction.controller';
 import { IntroductionService } from './introduction.service';
+import { UserModule } from '../user/user.module';
 
 @Module({
+  imports: [UserModule],
   controllers: [IntroductionController],
   providers: [IntroductionService]
 })

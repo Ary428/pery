2f92a0470f46ee400667c1e3365e8fc9279db3bf
Arye <72888630+Ary428@users.noreply.github.com>
2025-05-13 22:14:31 +0300

    implement GET /introduction/:articleName route with placeholder response

--- backend/src/introduction/introduction.controller.ts
+++ backend/src/introduction/introduction.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('introduction')
-export class IntroductionController {}
+export class IntroductionController {
+  @Get(':articleName')
+  getIntroduction(@Param('articleName') articleName: string) {
+    return {
+      scrapeDate: Date.now(),
+      articleName,
+      introduction: 'This is a placeholder.'
+    };
+  }
+}
